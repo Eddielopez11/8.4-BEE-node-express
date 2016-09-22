ab21fcfc1bef4fb36afaaf9538116622bf78ce25
Eduardo Lopez <eddielopez2101@gmail.com>
2016-09-22 13:41:02 -0500

    removed unnecessary test files and removed unwanted tests from home-test.js for boilerplate

--- __tests__/Home-test.js
+++ __tests__/Home-test.js
@@ -5,55 +5,6 @@ import Home from '../lib/Home'
 
 
 describe("Home", () => {
-  it("should have blogs", () => {
-    let mainRendered = TestUtils.renderIntoDocument(
-      <Home/>
-    )
-    // fake state for blogPosts
-    mainRendered.setState({
-      blogPosts: [
-         {
-          _id: "1738",
-          author: "Fetty",
-          timeStamp: "2016.09.16",
-          content: "Lorem",
-          title: "moms spaghetti",
-          user_info: {
-            user_id: "679",
-            user_name: "remys boys",
-            user_imgSrc: "remyboys.jpeg"
-          }
-        }
-      ]
-    })
-    // I want this to find an article
-    let articleEl = TestUtils.findRenderedDOMComponentWithClass(mainRendered,"article__previews")
-    expect(articleEl).toBeDefined()
-  })
 
-  it("sould link to specific article", () => {
-    let mainRendered = TestUtils.renderIntoDocument(
-      <Home/>
-    )
-    mainRendered.setState({
-      blogPosts: [
-         {
-          _id: "1738",
-          author: "Fetty",
-          timeStamp: "2016.09.16",
-          content: "Lorem",
-          title: "moms spaghetti",
-          user_info: {
-            user_id: "679",
-            user_name: "remys boys",
-            user_imgSrc: "remyboys.jpeg"
-          }
-        }
-      ]
-    })
-
-    let articleEl = TestUtils.findRenderedDOMComponentWithClass(mainRendered, "article__link")
-    expect(articleEl.getAttribute("href")).toBe("#/article/1738")
-  })
 
 })
